15f9809ab82b13371be8fe63dc43de59e3ef1ba8
batuhan38 <83165284+batuhan38@users.noreply.github.com>
2021-04-25 14:28:19 +0300

    Add project files.

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVeriYazdir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVeriYazdir
+            // 
+            this.btnVeriYazdir.Location = new System.Drawing.Point(154, 160);
+            this.btnVeriYazdir.Name = "btnVeriYazdir";
+            this.btnVeriYazdir.Size = new System.Drawing.Size(75, 23);
+            this.btnVeriYazdir.TabIndex = 0;
+            this.btnVeriYazdir.Text = "Döviz Kurları";
+            this.btnVeriYazdir.UseVisualStyleBackColor = true;
+            this.btnVeriYazdir.Click += new System.EventHandler(this.btnVeriYazdir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(53, 199);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(299, 186);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 411);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVeriYazdir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVeriYazdir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void main(String[] Args)
+        {
+           
+        }
+
+        private void btnVeriYazdir_Click(object sender, EventArgs e)
+        {
+            veri_yaz();
+            veri_oku();
+           
+        }
+
+        private void veri_yaz()
+        {
+            string bugün = "http://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldoc = new XmlDocument();
+            xmldoc.Load(bugün);
+            DateTime tarih = Convert.ToDateTime(xmldoc.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+
+            string USD = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/BanknoteSelling").InnerXml;
+            label1.Text = string.Format("Tarih{0} USD;{1}", tarih.ToShortDateString(), USD);
+
+            string EURO = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/BanknoteSelling").InnerXml;
+            label2.Text = string.Format("Tarih{0} EUR;{1}", tarih.ToShortDateString(), EURO);
+
+
+            string POUND = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/BanknoteSelling").InnerXml;
+            label3.Text = string.Format("Tarih{0} GBP;{1}", tarih.ToShortDateString(), POUND);
+
+
+            StreamWriter sw1 = File.AppendText("C:\\Users\\Nurcan\\Desktop\\dövizKurlari.txt");
+
+            sw1.WriteLine(label1.Text);
+            sw1.WriteLine(label2.Text);
+            sw1.WriteLine(label3.Text);
+
+            sw1.Close();
+        }
+
+        private void veri_oku()
+        {
+            StreamReader sr = File.OpenText("C:\\Users\\Nurcan\\Desktop\\dövizKurlari.txt");
+            string metin;
+          
+            while ((metin=sr.ReadLine())!=null)
+            {
+                
+                listBox1.Items.Add(metin);
+            }
+            sr.Close();
+            string[] metinList = new string[listBox1.Items.Count];
+            listBox1.Items.CopyTo(metinList, 0);
+            var metinList2 = metinList.Distinct();
+            listBox1.Items.Clear();
+            foreach (string s in metinList2)
+            {
+                listBox1.Items.Add(s);
+            }
+            System.IO.File.WriteAllLines("C:\\Users\\Nurcan\\Desktop\\dövizKurlari.txt", metinList2);
+        }
+
+    }
+}
